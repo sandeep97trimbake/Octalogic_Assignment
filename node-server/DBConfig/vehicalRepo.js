dc7e08a3abd4787fef706380531123304e8cac19
sandeep97trimbake <55341430+sandeep97trimbake@users.noreply.github.com>
2024-09-13 18:04:48 +0530

    API development for vehical

---
+++ node-server/DBConfig/vehicalRepo.js
@@ -0,0 +1,9 @@
+class vehicalRepo{
+    constructor(connectionPool){
+        this.connectionPool=connectionPool
+    }
+
+    get(type){}
+}
+
+module.exports = vehicalRepo;
--- node-server/routes/vehical.js
+++ node-server/routes/vehical.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const connectionPool = require("../DBConfig/connectionpool")
+const vehicalRepo = require("./../DBConfig/vehicalRepo")
+
+const repo =new vehicalRepo(connectionPool);
 
 /* GET vehical by type. */
 router.get('/:id', function(req, res, next) {
     console.log("ID : " , req.params.id);
     res.sendStatus(200)
+  res.send('get vehical');
+});
 
-
+/* GET vehical by type. */
+router.get('/:type', function(req, res, next) {
+    console.log("ID : " , req.params.type);
+    res.sendStatus(200)
   res.send('get vehical');
 });
 
