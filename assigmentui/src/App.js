fe35a082d284b50222a101483efb68303f16fc7d
sandeep97trimbake <55341430+sandeep97trimbake@users.noreply.github.com>
2024-09-14 01:08:02 +0530

    business logic development

--- assigmentui/src/App.js
+++ assigmentui/src/App.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import QuestionCard from './questions/questionCard';
 
 
 
 function App() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/question")
+      .then(respose => respose.json())
+      .then(data => setQuestions(data))
+      .catch(err => console.log(err))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <ul>
+        {
+          questions.map((list, index) => (
+            <>
+            <QuestionCard question = {list.question}></QuestionCard></>
+          ))
+        }
+      </ul>
     </div>
   );
 }
---
+++ assigmentui/src/questions/questionCard.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+
+
+const QuestionCard = ({ question }) => {
+    return (
+        <h1>
+            {question}
+        </h1>
+    )
+}
+
+export default QuestionCard
